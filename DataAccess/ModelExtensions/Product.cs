827611e525b90d0976964a9d55cd22cbed28a433
Bolty <stephen_bolt@yahoo.co.uk>
2016-05-11 09:28:26 +0100

    Add sample 'metadata' class for Product

---
+++ DataAccess/ModelExtensions/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess
+{
+    [MetadataType(typeof(MetadataProduct))]
+    public partial class Product
+    {
+    }
+
+    public class MetadataProduct
+    {
+        [Display(Name = "Category")]
+        public int CategoryId;
+    }
+}
